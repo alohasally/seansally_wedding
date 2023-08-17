576d8969247fba22767df6a10e484ec955204c2e
Sally Lee <sallylee@Sallyui-Macmini-2.local>
2023-08-17 09:01:21 +0900

    배포

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <div className="max-w-[390px] h-fit flex flex-col items-center justify-center mx-auto bg-black">
-  //   <App />
-  // </div>
-  <HashRouter basename={process.env.PUBLIC_URL}>
-    <div className="max-w-[390px] h-fit flex flex-col items-center justify-center mx-auto bg-black">
-      <Switch>
-        <Route path="/" element={<App />} />
-      </Switch>
-    </div>
-  </HashRouter>
+  <div className="max-w-[390px] h-fit flex flex-col items-center justify-center mx-auto bg-black">
+    <App />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
