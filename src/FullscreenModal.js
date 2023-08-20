7da4554b35ff65c85ed1c01481f2cac8bf353213
Sally Lee <sallylee@Sallyui-Macmini-2.local>
2023-08-20 23:44:20 +0900

    모달 이미지 위치 수정

--- src/FullscreenModal.js
+++ src/FullscreenModal.js
@@ -12,7 +12,7 @@ export default function FullScreenModal({ imageSrc, onClose }) {
 
   return (
     <div
-      className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-60 modal-background"
+      className="fixed top-1/2 -translate-y-1/2 z-50 flex justify-center items-center bg-black bg-opacity-60"
       onClick={handleBackgroundClick}
     >
       <div className="max-w-screen-lg w-full h-full relative">
